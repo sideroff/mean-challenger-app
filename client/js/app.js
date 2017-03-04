5a2592025a21ef2f3f654a65a95aa711dae04daa
sideroff <ivan.sideroff@gmail.com>
2017-03-04 11:25:06 +0200

    fixed: client side routes refactoring

--- client/js/app.js
+++ client/js/app.js
@@ -1,59 +1,78 @@
 var app = angular.module('meanStoreManagementSystem', ['ngResource', 'ngRoute', 'ngAnimate'])
 
-paths = {
-    '/': {
+let routes = [
+    {
+        route: '/',
+        href: '/',
+        linkText: 'Home',
         templateUrl: 'views/index.html',
         controller: 'homeController'
     },
-    '/challenges': {
-
+    {
+        route: '/challenges',
+        href: '/challenges',
+        linkText: 'Challenges',
         templateUrl: 'views/challenges.html',
         controller: 'challengesController'
     },
-    '/challenges/create': {
+    {
+        route: '/challenges/create',
+        href: '/challenges/create',
+        linkText: 'Create challenge',
         templateUrl: 'views/challengeCreationForm.html',
         controller: 'challengesController',
         requiredLogin: true
     },
-    '/register': {
+    {
+        route: '/register',
+        href: '/register',
+        linkText: 'Register',
         templateUrl: 'views/register.html',
         controller: 'usersController',
         requredLogout: true
     },
-    '/login': {
+    {
+        route: '/login',
+        href: '/login',
+        linkText: 'Login',
         templateUrl: 'views/login.html',
         controller: 'usersController',
         requiredLogout: true
     },
-    '/not-found': {
-        templateUrl: 'views/not-found.html'
-    }
-}
+    {
+        route: '/users/username',
+        href: '"/users/" + user.username',
+        linkText: 'user.username',
+        templateUrl: 'views/profile.html',
+        controller: 'usersController',
+        requiredLogout: true
+    }]
 
 app.config(['$routeProvider', '$locationProvider', ($routeProvider, $locationProvider) => {
 
-
     $locationProvider.html5Mode(true)
 
-    for (var path in paths) {
-        $routeProvider.when(path, paths[path])
+    for (let r of routes) {
+        $routeProvider.when(r.route, r)
     }
-    $routeProvider.otherwise({ redirectTo: '/not-found' })
+    $routeProvider
+        .when('/not-found', { templateUrl: 'views/not-found.html' })
+        .otherwise({ redirectTo: '/not-found' })
 
 }]).run(($rootScope, $location, userService, popupService) => {
-        $rootScope.$on('$routeChangeStart', (event, toState, toParams, fromState, fromParams) => {
-            let desiredPath = paths[toState.$$route.originalPath]
-            if (desiredPath) {
-                if (desiredPath.requiredLogin && !$rootScope.user) {
-                    popupService.addPopup({ type: 'error', text: 'You need to be logged in first!' })
-                    $location.path('/login')
-                    event.preventDefault()
-                }
-                else if (desiredPath.requiredLogout && $rootScope.user) {
-                    popupService.addPopup({ type: 'error', text: 'You need to be logged out first!' })
-                    $location.path('/')
-                    event.preventDefault()
-                }
+    $rootScope.$on('$routeChangeStart', (event, toState, toParams, fromState, fromParams) => {
+        let desiredPath = routes.find(r => r.route == toState.$$route.originalPath)
+        if (desiredPath) {
+            if (desiredPath.requiredLogin && !$rootScope.user) {
+                popupService.addPopup({ type: 'error', text: 'You need to be logged in first!' })
+                $location.path('/login')
+                event.preventDefault()
             }
-        })
+            else if (desiredPath.requiredLogout && $rootScope.user) {
+                popupService.addPopup({ type: 'error', text: 'You need to be logged out first!' })
+                $location.path('/')
+                event.preventDefault()
+            }
+        }
     })
+})
